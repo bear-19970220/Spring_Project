9ce076b21465cb21eea339d1d06bdda961d20695
bear_19970220 <bear_19970220@163.com>
2019-12-11 19:47:51 +0800

    IOC 三层架构搭建

--- Spring02_三层架构/src/test/java/UserTest.java
+++ Spring02_三层架构/src/test/java/UserTest.java
@@ -3,10 +3,13 @@ import com.dfbz.domain.User;
 import com.dfbz.service.UserService;
 import com.dfbz.util.JdbcUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.lang.reflect.InvocationHandler;
@@ -18,15 +21,17 @@ import java.lang.reflect.Proxy;
  * @company 东方标准
  * @date 2019/12/10 18:35
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring.xml")
 public class UserTest {
+
+    @Autowired
+    UserService userService;
 
     @Test
     public void test_dao() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
-        UserService userService = ac.getBean("userService", UserService.class);
-//        for (User user : userDao.findAllUser()) {
-//            System.out.println(user);
-//        }
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+//        UserService userService = ac.getBean("userService", UserService.class);
         userService.listUser().forEach(x -> System.out.println(x));
     }
 
